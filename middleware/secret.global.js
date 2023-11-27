50d5ba08842a7f1326289ccd4f0f6aa3e6266b88
mhtsos <dimitriskourgiantakis@gmail.com>
2023-11-27 02:52:35 +0200

    feat: Added secret middleware

---
+++ middleware/secret.global.js
@@ -0,0 +1,4 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  if (to.path !== '/secret')
+    return navigateTo('/secret')
+})
---
+++ pages/secret.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <div class="flex flex-col items-center justify-center h-screen">
+      <h1 class="text-4xl font-bold">
+        Oops! This page is under construction
+      </h1>
+      <h2 class="text-2xl font-bold">
+        It will be available soon
+      </h2>
+      <Icon name="emojione-monotone:see-no-evil-monkey" size="64" class="text-gray-800" />
+    </div>
+  </div>
+</template>
