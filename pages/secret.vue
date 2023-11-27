b1a98560f4104176c90397a14047f09e08b8fde8
mhtsos <dimitriskourgiantakis@gmail.com>
2023-11-27 02:56:51 +0200

    feat: changes a bit the secret page

--- pages/secret.vue
+++ pages/secret.vue
@@ -6,7 +6,10 @@
   <div>
     <div class="flex flex-col items-center justify-center h-screen">
       <h1 class="text-4xl font-bold">
-        Oops! This page is under construction
+        Oops!
+      </h1>
+      <h1 class="text-4xl font-bold">
+        This page is under construction
       </h1>
       <h2 class="text-2xl font-bold">
         It will be available soon
