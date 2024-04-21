191179231fb70fee5ca443c7209c329760910494
dimitriskourg <dimitriskourgiantakis@gmail.com>
2024-04-22 01:22:52 +0300

    fix: remove top margin in home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <script setup>
-import MainLayout from '~/layouts/MainLayout.vue';
-import { useUserStore } from '~/stores/user';
+import MainLayout from '~/layouts/MainLayout.vue'
+import { useUserStore } from '~/stores/user'
 
 const userStore = useUserStore()
 </script>
 
 <template>
   <MainLayout>
-    <div id="IndexPage" class="">
+    <div id="IndexPage" class="-mt-2">
       <div class="relative">
         <img src="/cbum.png" alt="C bum sitting" class="w-full min-h-full">
         <NuxtLink to="/products" class="absolute bottom-10 left-1/2 transform -translate-x-1/2 bg-white text-black px-4 py-3 font-semibold transition duration-500 hover:scale-110 drop-shadow-lg">
